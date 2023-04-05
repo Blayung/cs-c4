baf7eac136d3470da610837238029fb2d0d42297
Blayung <gpewojan1@gmail.com>
2023-04-05 21:07:45 +0200

    Change the way typing looks + a few tweaks to the code

--- sketch_c4.ino
+++ sketch_c4.ino
@@ -25,14 +25,14 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x3F,16,2);
 int i;
 char input;
 uint8_t cursor_pos=0;
-char code[7]={'#','#','#','#','#','#','#'}; // # = empty
+char code[7]={'*','*','*','*','*','*','*'}; // * = empty
 bool is_planted=false;
 int timer=0;
 int beep_timer=0;
 
 void clear_code(){ // Function that clears the bomb code
   for(i=0;i<7;i++){
-    code[i]='#';
+    code[i]='*';
   }
 }
 
@@ -58,7 +58,9 @@ void setup(){
   // LCD init
   lcd.init();
   lcd.backlight();
-  lcd.blink();
+  lcd.cursor();
+  lcd.print("    *******");
+  lcd.setCursor(4,0);
 }
   
 void loop(){
@@ -94,30 +96,29 @@ void loop(){
     }
   }
 
-  lcd.setCursor(cursor_pos,0);
+  lcd.setCursor(cursor_pos+4,0);
   input = keypad.getKey();
-  if (input){ // TODO: Make the typing work like in cs (the input field is filled with "*" and numbers appear from the right)
+  if (input){ // TODO: Make the typing work like in cs (the numbers appear from the right)
     if((input=='A')||(input=='B')||(input=='C')||(input=='D')){} // Empty buttons
     else if(input=='*'){ // The delete button
       if (cursor_pos!=0){
         cursor_pos--;
-        code[cursor_pos]='#';
-        lcd.setCursor(0,0);
+        code[cursor_pos]='*';
         lcd.clear();
+        lcd.setCursor(4,0);
         for(i=0;i<7;i++){
-          if(code[i]!='#'){
-            lcd.print(code[i]);
-          }
+          lcd.print(code[i]);
         }
       }
     }else if(input=='#'){ // The clear button
-      cursor_pos=0;
-      lcd.setCursor(0,0);
-      lcd.clear();
       clear_code();
+      cursor_pos=0;
+      lcd.clear();
+      lcd.setCursor(4,0);
+      lcd.print("*******");
     }else{ // A number was provided
-      lcd.print(input);
       code[cursor_pos]=input;
+      lcd.print(input);
 
       if(cursor_pos<6){
         cursor_pos++;
@@ -130,36 +131,45 @@ void loop(){
             timer=0;
             beep_timer=0;
             tone(BEEPER,700,1500);
-            lcd.setCursor(0,0);
             lcd.clear();
-            lcd.print("  THE BOMB HAS");
-            lcd.setCursor(0,1);
-            lcd.print("  BEEN DEFUSED!");
+            lcd.setCursor(2,0);
+            lcd.print("THE BOMB HAS");
+            lcd.setCursor(2,1);
+            lcd.print("BEEN DEFUSED!");
             delay(1500);
-            lcd.clear();
-            clear_code();
             digitalWrite(DIODE_R,LOW);
             digitalWrite(DIODE_G,LOW);
+            lcd.clear();
+            lcd.setCursor(4,0);
+            lcd.print("*******");
+            lcd.setCursor(4,0);
+            clear_code();
           }else{
             //PLANTED
             is_planted=true;
             digitalWrite(DIODE_R,HIGH);
             digitalWrite(DIODE_G,HIGH);
-            lcd.setCursor(0,0);
             lcd.clear();
-            lcd.print("  THE BOMB HAS");
-            lcd.setCursor(0,1);
-            lcd.print("  BEEN PLANTED!");
+            lcd.setCursor(2,0);
+            lcd.print("THE BOMB HAS");
+            lcd.setCursor(2,1);
+            lcd.print("BEEN PLANTED!");
             delay(1000);
             lcd.clear();
+            lcd.setCursor(4,0);
+            lcd.print("*******");
+            lcd.setCursor(4,0);
             clear_code();
           }
         }else{ // Invalid code
-          lcd.setCursor(0,0);
           lcd.clear();
-          lcd.print(" INVALID CODE!");
+          lcd.setCursor(1,0);
+          lcd.print("INVALID CODE!");
           delay(750);
           lcd.clear();
+          lcd.setCursor(4,0);
+          lcd.print("*******");
+          lcd.setCursor(4,0);
           clear_code();
         }
       }
