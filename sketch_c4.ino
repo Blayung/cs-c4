82cccfccb321f4cd56468fdbc8a2cdca715779cf
Blayung <gpewojan1@gmail.com>
2023-11-10 18:04:20 +0100

    refreshed the code, made the bomb more configurable (though you still configure it in the code), and changed the way the bomb figures out beeping speed to be more flexible

--- sketch_c4.ino
+++ sketch_c4.ino
@@ -1,183 +1,186 @@
-#include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
-// COMPILER CONSTANTS
-#define BEEPER A0
-#define DIODE_R A1
-#define DIODE_G A2
-#define DIODE_B A3
+// SETTINGS
+    // Pins
+    #define BUZZER A0
+    #define DIODE_R A1
+    #define DIODE_G A2
+    #define DIODE_B A3
+    #define KEYPAD_ROW_PINS {7, 6, 5, 4}
+    #define KEYPAD_COL_PINS {11, 10, 9, 8}
+    // The LCD Address
+    #define LCD_ADDRESS 0x3F
+    // Durations, delays and sound frequencies (all values are in either hertz or milliseconds)
+    #define EXPLOSION_DIODE_DURATION 3000
+    #define EXPLOSION_SOUND_FREQUENCY 100
+    #define EXPLOSION_SOUND_DURATION 2750
+    #define BEEP_DIODE_DURATION 100
+    #define BEEP_SOUND_FREQUENCY 440
+    #define BEEP_SOUND_DURATION 250
+    #define BOMB_PLANTED_DURATION 1000
+    #define BOMB_DEFUSED_DURATION 1500
+    #define BOMB_DEFUSED_SOUND_FREQUENCY 700
+    #define BOMB_DEFUSED_SOUND_DURATION 1500
+    #define TYPING_SOUND_FREQUENCY 200
+    #define TYPING_SOUND_DURATION 100
+    #define INVALID_CODE_DURATION 750
+    #define INVALID_CODE_SOUND_FREQUENCY 150
+    #define INVALID_CODE_SOUND_DURATION 700
+    // The bomb code
+    #define CODE_1 '7'
+    #define CODE_2 '3'
+    #define CODE_3 '5'
+    #define CODE_4 '5'
+    #define CODE_5 '6'
+    #define CODE_6 '0'
+    #define CODE_7 '8'
+    // Other
+    #define EXPLOSION_TIME 40000 // Milliseconds
+    #define BEEPING_SPEED 2000
 
-// THE KEYPAD
-char keypad_hexa_keys[4][4] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(LCD_ADDRESS, 16, 2);
+
+char keypadHexaKeys[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
 };
-byte keypad_row_pins[4] = {7, 6, 5, 4}; // ROW PINS
-byte keypad_col_pins[4] = {11, 10, 9, 8}; // COLUMN PINS
-Keypad keypad = Keypad(makeKeymap(keypad_hexa_keys), keypad_row_pins, keypad_col_pins, 4, 4);
+byte keypadRowPins[4] = KEYPAD_ROW_PINS;
+byte keypadColPins[4] = KEYPAD_COL_PINS;
+Keypad keypad = Keypad(makeKeymap(keypadHexaKeys), keypadRowPins, keypadColPins, 4, 4);
 
-// THE LCD SCREEN
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x3F,16,2);
+uint8_t cursorPos = 0;
+char code[7] = {'*', '*', '*', '*', '*', '*', '*'}; // * = empty
+bool isPlanted = false;
+unsigned long scheduledExplosion;
+unsigned long previousBeep;
 
-// NORMAL VARS
-int i;
-char input;
-uint8_t cursor_pos=0;
-char code[7]={'*','*','*','*','*','*','*'}; // * = empty
-bool is_planted=false;
-int timer=0;
-int beep_timer=0;
-
-void clear_code(){ // Function that clears the bomb code
-  for(i=0;i<7;i++){
-    code[i]='*';
-  }
-}
-
-void beep(){ // What happens when the bomb beeps
-  beep_timer=0;
-  tone(BEEPER,440,250);
-  digitalWrite(DIODE_R,LOW);
-  delay(100);
-  digitalWrite(DIODE_R,HIGH);
-}
-
-void setup(){
-  // Pins
-  pinMode(BEEPER,OUTPUT);
-  digitalWrite(BEEPER,LOW);
-  pinMode(DIODE_R,OUTPUT);
-  digitalWrite(DIODE_R,LOW);
-  pinMode(DIODE_G,OUTPUT);
-  digitalWrite(DIODE_G,LOW);
-  pinMode(DIODE_B,OUTPUT);
-  digitalWrite(DIODE_B,HIGH);
-
-  // LCD init
-  lcd.init();
-  lcd.backlight();
-  lcd.cursor();
-  lcd.setCursor(4,0);
-  lcd.print("*******");
-  lcd.setCursor(4,0);
-}
-  
-void loop(){
-  if(is_planted){ //If the bomb is planted
-    timer++;
-    beep_timer++;
-
-    // Beeping
-    if(timer<5001 && beep_timer>1000){
-      beep();
-    }else if(timer>5000 && timer<10001 && beep_timer>800){
-      beep();
-    }else if(timer>10000 && timer<15001 && beep_timer>600){
-      beep();
-    }else if(timer>15000 && timer<20001 && beep_timer>400){
-      beep();
-    }else if(timer>20000 && timer<22501 && beep_timer>200){
-      beep();
-    }else if(timer>22500 && beep_timer>100){
-      beep();
+void clearCode() {
+    for (int i = 0; i < 7; i++) {
+        code[i] = '*';
     }
+}
 
-    if(timer>25000){ // The bomb goes off
-      is_planted=false;
-      timer=0;
-      beep_timer=0;
-      tone(BEEPER,100,2750);
-      digitalWrite(DIODE_R,LOW);
-      digitalWrite(DIODE_G,LOW);
-      digitalWrite(DIODE_B,LOW);
-      delay(3000);
-      digitalWrite(DIODE_B,HIGH);
-    }
-  }
+void setup() {
+    pinMode(BUZZER, OUTPUT);
+    digitalWrite(BUZZER, LOW);
+    pinMode(DIODE_R, OUTPUT);
+    digitalWrite(DIODE_R, LOW);
+    pinMode(DIODE_G, OUTPUT);
+    digitalWrite(DIODE_G, LOW);
+    pinMode(DIODE_B, OUTPUT);
+    digitalWrite(DIODE_B, HIGH);
 
-  lcd.setCursor(cursor_pos+4,0);
-  input = keypad.getKey();
-  if (input){
-    if((input=='A')||(input=='B')||(input=='C')||(input=='D')){} // Empty buttons
-    else if(input=='*'){ // The delete button
-      tone(BEEPER,200,100);
-      if (cursor_pos!=0){
-        cursor_pos--;
-        code[cursor_pos]='*';
-        lcd.clear();
-        lcd.setCursor(4,0);
-        for(i=0;i<7;i++){
-          lcd.print(code[i]);
+    lcd.init();
+    lcd.backlight();
+    lcd.cursor();
+    lcd.setCursor(4, 0);
+    lcd.print("*******");
+    lcd.setCursor(4, 0);
+}
+
+void loop() {
+    if (isPlanted) {
+        unsigned long currentMillis = millis();
+
+        if (currentMillis >= scheduledExplosion) {
+            isPlanted = false;
+            tone(BUZZER, EXPLOSION_SOUND_FREQUENCY, EXPLOSION_SOUND_DURATION);
+            digitalWrite(DIODE_R, LOW);
+            digitalWrite(DIODE_G, LOW);
+            digitalWrite(DIODE_B, LOW);
+            delay(EXPLOSION_DIODE_DURATION);
+            digitalWrite(DIODE_B, HIGH);
+        } else if (currentMillis - previousBeep >= max((scheduledExplosion - currentMillis) * BEEPING_SPEED / EXPLOSION_TIME, max(BEEP_SOUND_DURATION, BEEP_DIODE_DURATION) + 10)) {
+            previousBeep = currentMillis;
+            tone(BUZZER, BEEP_SOUND_FREQUENCY, BEEP_SOUND_DURATION);
+            digitalWrite(DIODE_R, LOW);
+            delay(BEEP_DIODE_DURATION);
+            digitalWrite(DIODE_R, HIGH);
         }
-      }
-    }else if(input=='#'){ // The clear button
-      tone(BEEPER,200,100);
-      clear_code();
-      cursor_pos=0;
-      lcd.clear();
-      lcd.setCursor(4,0);
-      lcd.print("*******");
-    }else{ // A number was provided
-      tone(BEEPER,200,100);
-      code[cursor_pos]=input;
-      lcd.print(input);
-
-      if(cursor_pos<6){
-        cursor_pos++;
-      }else{
-        cursor_pos=0;
-        if((code[0]=='7')&&(code[1]=='3')&&(code[2]=='5')&&(code[3]=='5')&&(code[4]=='6')&&(code[5]=='0')&&(code[6]=='8')){ // Valid code
-          if(is_planted){
-            //DEFUSED
-            is_planted=false;
-            timer=0;
-            beep_timer=0;
-            tone(BEEPER,700,1500);
-            lcd.clear();
-            lcd.setCursor(2,0);
-            lcd.print("THE BOMB HAS");
-            lcd.setCursor(2,1);
-            lcd.print("BEEN DEFUSED!");
-            delay(1500);
-            digitalWrite(DIODE_R,LOW);
-            digitalWrite(DIODE_G,LOW);
-            lcd.clear();
-            lcd.setCursor(4,0);
-            lcd.print("*******");
-            lcd.setCursor(4,0);
-            clear_code();
-          }else{
-            //PLANTED
-            is_planted=true;
-            digitalWrite(DIODE_R,HIGH);
-            digitalWrite(DIODE_G,HIGH);
-            lcd.clear();
-            lcd.setCursor(2,0);
-            lcd.print("THE BOMB HAS");
-            lcd.setCursor(2,1);
-            lcd.print("BEEN PLANTED!");
-            delay(1000);
-            lcd.clear();
-            lcd.setCursor(4,0);
-            lcd.print("*******");
-            lcd.setCursor(4,0);
-            clear_code();
-          }
-        }else{ // Invalid code
-          tone(BEEPER,150,700);
-          lcd.clear();
-          lcd.setCursor(1,0);
-          lcd.print("INVALID CODE!");
-          delay(750);
-          lcd.clear();
-          lcd.setCursor(4,0);
-          lcd.print("*******");
-          lcd.setCursor(4,0);
-          clear_code();
-        }
-      }
     }
-  }
+
+    lcd.setCursor(cursorPos + 4, 0);
+    char input = keypad.getKey();
+    if (input) {
+        if (input == '*') {
+            tone(BUZZER, TYPING_SOUND_FREQUENCY, TYPING_SOUND_DURATION);
+            if (cursorPos != 0) {
+                cursorPos--;
+                code[cursorPos] = '*';
+                lcd.clear();
+                lcd.setCursor(4, 0);
+                for (int i = 0; i < 7; i++) {
+                    lcd.print(code[i]);
+                }
+            }
+        } else if (input == '#') {
+            tone(BUZZER, TYPING_SOUND_FREQUENCY, TYPING_SOUND_DURATION);
+            clearCode();
+            cursorPos = 0;
+            lcd.clear();
+            lcd.setCursor(4, 0);
+            lcd.print("*******");
+        } else {
+            tone(BUZZER, TYPING_SOUND_FREQUENCY, TYPING_SOUND_DURATION);
+            code[cursorPos] = input;
+            lcd.print(input);
+
+            if (cursorPos < 6) {
+                cursorPos++;
+            } else {
+                cursorPos = 0;
+                if (code[0] == CODE_1 && code[1] == CODE_2 && code[2] == CODE_3 && code[3] == CODE_4 && code[4] == CODE_5 && code[5] == CODE_6 && code[6] == CODE_7) {
+                    if (isPlanted) {
+                        isPlanted = false;
+                        clearCode();
+                        tone(BUZZER, BOMB_DEFUSED_SOUND_FREQUENCY, BOMB_DEFUSED_SOUND_DURATION);
+                        lcd.clear();
+                        lcd.setCursor(2, 0);
+                        lcd.print("THE BOMB HAS");
+                        lcd.setCursor(2, 1);
+                        lcd.print("BEEN DEFUSED!");
+                        delay(BOMB_DEFUSED_DURATION);
+                        digitalWrite(DIODE_R, LOW);
+                        digitalWrite(DIODE_G, LOW);
+                        lcd.clear();
+                        lcd.setCursor(4, 0);
+                        lcd.print("*******");
+                        lcd.setCursor(4, 0);
+                    } else {
+                        isPlanted = true;
+                        clearCode();
+                        digitalWrite(DIODE_R, HIGH);
+                        digitalWrite(DIODE_G, HIGH);
+                        lcd.clear();
+                        lcd.setCursor(2, 0);
+                        lcd.print("THE BOMB HAS");
+                        lcd.setCursor(2, 1);
+                        lcd.print("BEEN PLANTED!");
+                        delay(BOMB_PLANTED_DURATION);
+                        unsigned long currentMillis = millis();
+                        previousBeep = currentMillis;
+                        scheduledExplosion = currentMillis + EXPLOSION_TIME;
+                        lcd.clear();
+                        lcd.setCursor(4, 0);
+                        lcd.print("*******");
+                        lcd.setCursor(4, 0);
+                    }
+                } else {
+                    clearCode();
+                    tone(BUZZER, INVALID_CODE_SOUND_FREQUENCY, INVALID_CODE_SOUND_DURATION);
+                    lcd.clear();
+                    lcd.setCursor(1, 0);
+                    lcd.print("INVALID CODE!");
+                    delay(INVALID_CODE_DURATION);
+                    lcd.clear();
+                    lcd.setCursor(4, 0);
+                    lcd.print("*******");
+                    lcd.setCursor(4, 0);
+                }
+            }
+        }
+    }
 }
